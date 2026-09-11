58804926a576dd8b33befcdff2cf4b286037ca3c
Pranav M <pranavm132@gmail.com>
2026-09-11 23:17:21 +0530

    Add Easy C++ Solution for 136. Single Number from LeetCode

--- LeetCode/C++/Easy/136. Single Number/Solution.cpp
+++ LeetCode/C++/Easy/136. Single Number/Solution.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result = 0;
+        int n = nums.size();
 
-        for (int num : nums) {
-            result ^= num;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return nums[i];
+            }
         }
 
-        return result;
+        return -1; 
     }
 };
