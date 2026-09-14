ec0e5fc9b46a561c097199a39a5edbb3aace54d3
Pranav M <pranavm132@gmail.com>
2026-09-14 21:42:49 +0530

    Add Medium Python Solution for 75. Sort Colors from LeetCode

---
+++ LeetCode/Python/Medium/75. Sort Colors/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sortColors(self, nums):
+        count0 = count1 = count2 = 0
+        for num in nums:
+            if num == 0:
+                count0 += 1
+            elif num == 1:
+                count1 += 1
+            else:
+                count2 += 1
+        index = 0
+        for _ in range(count0):
+            nums[index] = 0
+            index += 1
+        for _ in range(count1):
+            nums[index] = 1
+            index += 1
+        for _ in range(count2):
+            nums[index] = 2
+            index += 1
+        
